ec2eee7541d8237bbc212034ac5540d93327e6e3
Rodrigo Bastos <rdgbastos@icloud.com>
2023-06-06 02:58:55 -0300

    adicionando redes sociais e informações

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,16 @@
+import SocialNetworks from './SocialNetworks';
+
 import Avatar from '../img/foto.jpg';
 import '../styles/components/sidebar.sass';
+import InformationContainer from './InformationContainer';
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img src={Avatar} alt="Rodrigo Bastos" />
       <p className="title">Desenvolvedor</p>
-      <p>redes sociais</p>
-      <p>iformações de contato</p>
+      <SocialNetworks />
+      <InformationContainer />
       <a href="" className="btn">Download Currículo</a>
     </aside>
   )
