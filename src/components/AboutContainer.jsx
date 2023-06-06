cf8375d6d430359fdc3902c3d8bc828cc68b63c2
Rodrigo Bastos <rdgbastos@icloud.com>
2023-06-06 03:34:02 -0300

    projeto finalizado

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,11 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, vitae numquam. Hic pariatur unde autem, dolor iste, nemo natus in, reprehenderit quia quaerat consequuntur dolore facere? Beatae ad dolor mollitia!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt iure sapiente quos quo officiis dolor ad saepe tempore dolorem eum vero deleniti a rem libero, accusantium placeat reprehenderit odit necessitatibus.</p>
+    </section>
+  )
+}
+
+export default AboutContainer;
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,11 @@
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis vel cumque consectetur! Soluta harum rem aliquid vel nemo? Recusandae, veniam dolorum. Numquam tempore consectetur consequuntur nisi quam, aliquid ea sunt?</p>
+      <a href="#" className="btn">Ver Projetos</a>
+    </section>
+  )
+}
+
+export default ProjectsContainer;
